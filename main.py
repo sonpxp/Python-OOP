39cad635df56d5ac2ede8e7e944b658d60927d21
sonpxp <sonmob202@gmail.com>
2021-10-15 00:17:35 +0700

    constructor, __init__

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 class Item:
     # can set default = 0 ex: quality = 0
     pay_rate = 0.8
+    all = []
 
     def __init__(self, name, price, quality):
         # run validations to the received arguments
@@ -12,21 +13,31 @@ class Item:
         self.price = price
         self.quality = quality
 
+        # Actions to execute
+        Item.all.append(self)
+
     def calculator_total_price(self):
         return self.price * self.quality
 
     def apply_discount(self):
         self.price = self.price * self.pay_rate  # Item.pay_rate
 
+    def __repr__(self):
+        return f"Item '{self.name}, {self.price}, {self.quality}'"
+
 
 item1 = Item("Phone", 100, 5)
 item1.apply_discount()
 print(item1.price)
 
-item2 = Item("Phone", 300, 2)
+item2 = Item("Samsung", 300, 2)
 item2.pay_rate = 0.5  # self.pay_rate
 item2.apply_discount()
 print(item2.price)
+
+print(Item.all)
+# for instance in Item.all:
+#     print(instance.name)
 
 # item1 = Item("Phone", 100, 5)
 # item2 = Item("Laptop", 1000, 4)
